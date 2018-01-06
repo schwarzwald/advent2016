0cb8c1d835f66b5fcad6015e1e76bcb3482aa2ab
bibo <bibaso@gmail.com>
2018-01-06 16:01:46 +0100

    Day 18

---
+++ day18/part1.js
@@ -0,0 +1,12 @@
+module.exports = input => {
+  input = [...input].map(t => t == '^');
+  let sum = 0;
+  let rows = 40;
+
+  while (rows--) {
+    sum += input.filter(b => !b).length;
+    input = input.map((bit, i) => input[i - 1] ^ input[i + 1]);
+  }
+
+  return sum;
+}
